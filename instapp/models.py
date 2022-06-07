eb7fe68f97631a1c8a6ba0698d8d949005ba0c1d
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-07 13:29:29 +0300

    function: Save, delete and update - adds functions to edit instagram post

--- instapp/models.py
+++ instapp/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Insta_Images(models.Model):
+class Images(models.Model):
     image_name = models.CharField(max_length=100)
     image_caption = models.CharField(max_length=10000)
-    image_url = models.ImageField(upload_to='images/')
+    image_url = models.ImageField(upload_to='instagram/')
     image_likes = models.IntegerField(default=0)
-    image_comments = models.IntegerField(default=0)
+    image_comments = models.TextField( blank=True)
 
     def __str__(self):
         return self.image_name
+    
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+    
+    def update_caption(self, new_caption):
+        self.image_caption = new_caption
+        self.save()
