8f8e5c67198fb1208b75292588458e286a3a35fa
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-07 09:08:21 +0300

    perf: View Function - Adds view function to help render the form

--- instapp/views.py
+++ instapp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from instapp.forms import NewInstaImages
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def insta_post(request):
+    if request.method == 'POST':
+        form = NewInstaImages(request.POST, request.FILES)
+        if form.is_valid():
+            instapost= form.save()
+            instapost.save()
+        return redirect ('index')
+    else:
+        form=NewInstaImages
+    
+    return render (request, 'post.html', {"form": form})
+
+    
