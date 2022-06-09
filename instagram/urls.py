eace9fd7e9088ad70beffe578cc3c4caa10625bd
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-06-09 21:28:38 +0300

    fix-bug:Corrects login logout mulfunction

--- instagram/urls.py
+++ instagram/urls.py
@@ -28,7 +28,7 @@ from django_registration.backends.one_step.views import RegistrationView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('instapp.urls')),
-    path('accounts/register/', RegistrationView.as_view(success_url='accounts/register/'),name='django_registration_register'),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'),name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', views.logout_then_login, name='logout')
--- instapp/views.py
+++ instapp/views.py
@@ -1,16 +1,34 @@
+from multiprocessing import context
 from django.shortcuts import redirect, render
-
+from django.contrib.auth.decorators import login_required
 from instapp.forms import NewInstaImages, NewInstaProfile
 from instapp.models import Images, Profile
+from django.contrib.auth.models import User
+
+
 
 # Create your views here.
+# @login_required(login_url='/accounts/login/')
 def index(request):
-    images= Images.objects.all()
-    return render(request, 'index.html', {"images": images})
+    
+    current_user = request.user
+    images = Images.objects.all()
 
+    try:
+        user = User.objects.get(username = current_user.username)
+        users = User.objects.exclude(username=current_user.username).exclude(is_superuser=True)
+    except:
+        user = None
+        users = None
+    
+    return render(request, 'index.html', {"images": images, "user": user, "users": users})
+
+
+@login_required(login_url='/accounts/login/')
 def newPost(request):
     return render(request, 'post.html')
 
+@login_required(login_url='/accounts/login/')
 def insta_post(request):
     if request.method == 'POST':
         form = NewInstaImages(request.POST, request.FILES)
@@ -23,6 +41,7 @@ def insta_post(request):
     
     return render (request, 'forms/new_post.html', {"form": form})
 
+@login_required(login_url='/accounts/login/')
 def update_profile(request):
     if request.method == 'POST':
         form = NewInstaProfile(request.POST, request.FILES)
@@ -35,6 +54,7 @@ def update_profile(request):
 
     return render(request, 'forms/addprofile.html', {"form": form})
 
+@login_required(login_url='/accounts/login/')
 def profile(request):
     profile = Profile.objects.all()
     images = Images.objects.all()
